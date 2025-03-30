296f7000bae02a2122e699efe5e8f77fa5eb1783
Gabriel <gl9736387@gmail.com>
2025-03-29 23:42:33 -0300

    DESAFIO DIO KOTLIN

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+data class Usuario(val nome: String,val idade: Int, val nivel: Nivel, val feedback : Int, val conteudoUsuario : List<ConteudoEducacional>){
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun avalicao(): String {
+         return when(feedback){
+            1 -> "BOM"
+            2 -> "EXCELENTE"
+            else -> "PESSIMO"
+        }
     }
 }
 
+data class ConteudoEducacional(var conteudo: Int, val duracao: Int = 60){
+
+    fun materia(): String{
+        return when(conteudo){
+            1 ->"MATEMATICA"
+            2 ->"PHYTON"
+            3 ->"JAVA SPRING BOOT"
+            4 ->"IA"
+            else -> "CONTEUDO NÃO EXISTE "
+
+        }
+    }
+    }
+
+  data class Formacao(var nome: String,var conteudos: List<ConteudoEducacional>) {
+
+      val inscritos = mutableListOf<Usuario>()
+    
+      fun matricular(vararg usuario: Usuario) {
+        inscritos.addAll(usuario.toList())
+      }
+  }
+
+  fun exibirUser(usuario : Usuario){
+    println("Usuário ${usuario.nome}, tem ${usuario.idade} anos, está no nivel ${usuario.nivel}, e o seu feedback é ${usuario.avalicao()}, está estudando o conteudo ${usuario.conteudoUsuario[0].materia()}\n\n")
+
+  }
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+
+    val pessoa = Usuario("Gabriel",20,Nivel.BASICO,1,listOf(ConteudoEducacional(2)))
+     exibirUser(pessoa)
+ 
+    val pessoa2 = Usuario("Ana",22,Nivel.AVANÇADO,1,listOf(ConteudoEducacional(1)))
+     exibirUser(pessoa2)
+
+    val formacao = Formacao("Kotlin",listOf(ConteudoEducacional(2), ConteudoEducacional(1)))
+    formacao.matricular(pessoa,pessoa2)
+
+    println("Conteúdos da formação ${formacao.nome}: ${formacao.conteudos.joinToString(", ") { it.materia() }}")
+
+
 }
+
